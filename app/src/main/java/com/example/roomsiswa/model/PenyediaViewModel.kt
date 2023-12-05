3336277bfc8e3a9d2d0912ad361881fcfc718229
fahmarosyidah <fahmarosyi@gmail.com>
2023-12-05 21:34:29 +0700

    menambahkan fungsi CreationExtras.aplikasiSiswa() di PenyediaViewModel

--- app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.roomsiswa.model
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.example.roomsiswa.AplikasiSiswa
+
+/**
+ * Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah instance dari [AplikasiSiswa].
+ */
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
