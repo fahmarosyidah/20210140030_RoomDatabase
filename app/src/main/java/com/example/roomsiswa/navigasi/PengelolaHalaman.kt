4ff944bcc7e7e32e1eb4d24b0e23f07afd1899be
fahmarosyidah <fahmarosyi@gmail.com>
2023-12-06 05:23:35 +0700

    menambahkan fungsi HostNavigasi di PengelolaHalaman

--- app/src/main/java/com/example/roomsiswa/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/roomsiswa/navigasi/PengelolaHalaman.kt
@@ -1,2 +1,32 @@
 package com.example.roomsiswa.navigasi
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.roomsiswa.ui.theme.halaman.DestinasiEntry
+import com.example.roomsiswa.ui.theme.halaman.DestinasiHome
+import com.example.roomsiswa.ui.theme.halaman.EntrySiswaScreen
+import com.example.roomsiswa.ui.theme.halaman.HomeScreen
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) }
+            )
+        }
+        composable(DestinasiHome.route){
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+    }
+}
