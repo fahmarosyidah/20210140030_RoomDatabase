4ff106962f2e871c92232b865b9a7b571936b029
fahmarosyidah <fahmarosyi@gmail.com>
2023-12-05 21:42:29 +0700

    menambahkan interface DestinasiNavigasi

---
+++ app/src/main/java/com/example/roomsiswa/navigasi/DestinasiNavigasi.kt
@@ -0,0 +1,14 @@
+package com.example.roomsiswa.navigasi
+
+interface DestinasiNavigasi {
+
+    /**
+     * Nama unik untuk menentukan jalur untuk composable
+     */
+    val route: String
+
+    /**
+     * String resource id yang berisi judul yang akan ditampilkan di layar halaman.
+     */
+    val titleRes: Int
+}
